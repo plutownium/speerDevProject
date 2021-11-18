5d2049ffbb4a46ac3a12848b5b3a23d36c79f948
Roland Mackintosh <rolandmackintosh@outlook.com>
2021-11-18 13:24:26 -0800

    made bug in clock display; mistake

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,33 @@ import Phone from "../img/phone.png";
 
 import "./Card.css";
 
+function convertTwentyFourHourClockToTwelveHour(militaryTime) {
+  let twelveHourVersion = "";
+  let hourPart = militaryTime.split(":")[0];
+  let asTwelveHourClock = hourPart - 12;
+  if (asTwelveHourClock <= 9) {
+    twelveHourVersion = twelveHourVersion + "0" + asTwelveHourClock.toString();
+  } else {
+    twelveHourVersion = twelveHourVersion + asTwelveHourClock.toString();
+  }
+  let minutesString = twelveHourVersion + militaryTime.split(":")[1];
+  twelveHourVersion = twelveHourVersion + ":" + minutesString;
+  return twelveHourVersion;
+}
+
+function convertToMilitaryTime(created_at) {
+  return created_at
+    .split("T")[1]
+    .split(".")[0]
+    .split(":")
+    .slice(0, 2)
+    .join(":"); // yes I love a good one liner
+}
+
 function Card({ created_at, number, recipient }) {
   let date = created_at.split("T")[0];
-  let timeCode = created_at.split("T")[1].split(".")[0];
+  let timeCodeAs24HrClock = convertToMilitaryTime(created_at);
+  let timeCode = convertTwentyFourHourClockToTwelveHour(timeCodeAs24HrClock);
   console.log(9, date, timeCode);
   return (
     <div className="cardMainContainer">
